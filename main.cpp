eefcc01d07d686203900da6d036475d7eb8c7407
Van Hour <vannhour15@gmail.com>
2026-06-10 21:15:35 +0700

    Main strcuture

--- main.cpp
+++ main.cpp
@@ -1,1 +1,16 @@
-/// Hi
+#include <iostream>
+using namespace std;
+
+int func1() {
+    int x = 3;
+    int y = 2;
+    int z = x + y;
+
+    return z;
+}
+
+int main() {
+    cout << func1() << endl;
+
+    return 0;
+};
